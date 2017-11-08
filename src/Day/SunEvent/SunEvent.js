283363df6b35f11c765785f5e6393b06b3eaa73a
Henri Ruoho <hruoho@l946.core.reaktor.fi>
2017-11-08 14:37:19 +0200

    draft timeline

--- src/Day/SunEvent/SunEvent.js
+++ src/Day/SunEvent/SunEvent.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './SunEvent.css';
 import moment from 'moment';
 
 class SunEvent extends Component {
 
-   render() {
-      return (
-        <div className="SunEvent">
-          <div className="eventType">{this.props.type}</div>
-          <div className="eventMoment">{moment(this.props.time).format('HH:mm:ss')}</div>
-          <div className="eventDiff">{this.props.diff}</div>
-          <p>active: {String(this.props.isActive)}</p>
+  getActiveClass() {
+    let classes = ['sunEvent', 'pure-g']
+    if (this.props.isActive)
+      classes.push('active')
+    return classes.join(' ')
+  }
+
+  render() {
+    return (<div className={this.getActiveClass()}>
+      <div className="pure-u-1-3">
+        <div className="eventType">
+          <p>{this.props.type}</p>
         </div>
-      );
-    }
+      </div>
+      <div className="pure-u-2-3">
+        <div className="eventMoments">
+          <p className="eventMoment">{moment(this.props.time).format('HH:mm:ss')}</p>
+          <p className="eventDiff">{this.props.diff}</p>
+        </div>
+      </div>
+    </div>);
+  }
 }
 
 export default SunEvent;
