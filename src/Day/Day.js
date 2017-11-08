4c7c4b3c2b98fb3900e187c14ccc7fb11b389514
Henri Ruoho <hruoho@l946.core.reaktor.fi>
2017-11-08 11:26:19 +0200

    bump

--- src/Day/Day.js
+++ src/Day/Day.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import './Day.css';
 import SunEvent from './SunEvent/SunEvent';
-import SunCalc from 'suncalc';
 import moment from 'moment';
 
+var SunCalc = require('suncalc')
+
 class Day extends Component {
+  constructor(props) {
+    super(props)
+    let latitude = 60.10
+    let longitude = 24.56
+    this.times = SunCalc.getTimes(moment(), latitude, longitude)
+  }
+
   render() {
     return (
       <div className="Day">
       {
-        ['nadir', 'sunrise', 'solarNoon', 'sunset'].map(type => <SunEvent type={type}/>)
+        ['nadir', 'sunrise', 'solarNoon', 'sunset'].map(type => <SunEvent type={type} times={this.times[type]}/>)
       }
       </div>
     );
--- src/Day/SunEvent/SunEvent.js
+++ src/Day/SunEvent/SunEvent.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import './SunEvent.css';
+import moment from 'moment';
 
 class SunEvent extends Component {
 
-  render() {
-    return (
-      <div className="SunEvent">
-      Hello from SunEvent/{this.props.type}
-      </div>
-    );
-  }
+    constructor(props) {
+      super(props)
+      this.time = moment(props.times)
+    }
+
+    getDiff() {
+      return moment().diff(this.time)
+    }
+
+    render() {
+
+      return (
+        <div className="SunEvent">
+          <div>Type: {this.props.type}</div>
+          <div>At: {moment(this.props.times).format('HH:mm:ss')}</div>
+          <div>Diff: {this.getDiff()}</div>
+        </div>
+      );
+    }
 }
 
 export default SunEvent;
