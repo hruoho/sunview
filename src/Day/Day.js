a54ee14032ccb19152179eeaeaa81917dd69c4ad
Anssi Kinnunen <anssikin@l1090.core.reaktor.fi>
2017-11-08 12:08:53 +0200

    move date interval logic to day component

--- src/Day/Day.js
+++ src/Day/Day.js
@@ -11,13 +11,38 @@ class Day extends Component {
     let latitude = 60.10
     let longitude = 24.56
     this.times = SunCalc.getTimes(moment(), latitude, longitude)
+    this.state = {
+      now: moment()
+    }
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      now: moment()
+    });
+  }
+
+  getDiff(type) {
+    const time = this.times[type]
+    return moment(this.state.now).to(time)
   }
 
   render() {
     return (
       <div className="Day">
       {
-        ['nadir', 'sunrise', 'solarNoon', 'sunset'].map(type => <SunEvent type={type} time={this.times[type]}/>)
+        ['nadir', 'sunrise', 'solarNoon', 'sunset'].map(type => <SunEvent type={type} time={this.times[type]} diff={this.getDiff(type)} />)
       }
       </div>
     );
--- src/Day/SunEvent/SunEvent.js
+++ src/Day/SunEvent/SunEvent.js
@@ -4,40 +4,12 @@ import moment from 'moment';
 
 class SunEvent extends Component {
 
-    constructor(props) {
-      super(props)
-      this.state = {
-        now: moment()
-      }
-    }
-
-    componentDidMount() {
-      this.timerID = setInterval(
-        () => this.tick(),
-        1000
-      );
-    }
-
-    componentWillUnmount() {
-      clearInterval(this.timerID);
-    }
-
-    tick() {
-      this.setState({
-        now: moment()
-      });
-    }
-
-    getDiff() {
-      return moment(this.state.now).to(this.props.time)
-    }
-
-    render() {
+   render() {
       return (
         <div className="SunEvent">
           <div className="eventType">{this.props.type}</div>
           <div className="eventMoment">{moment(this.props.time).format('HH:mm:ss')}</div>
-          <div className="eventDiff">{this.getDiff()}</div>
+          <div className="eventDiff">{this.props.diff}</div>
         </div>
       );
     }
