c327e324565ac4c3e9201389b43e6edc2ecfbbd1
Henri Ruoho <hruoho@l946.core.reaktor.fi>
2017-11-09 12:59:59 +0200

    translate events

--- src/Day/SunEvent/SunEvent.js
+++ src/Day/SunEvent/SunEvent.js
@@ -2,7 +2,19 @@ import React, {Component} from 'react';
 import './SunEvent.css';
 import moment from 'moment';
 
+const evtDict = {
+  nadir: 'Nadir',
+  sunrise: 'Sunrise',
+  solarNoon: 'Solar noon',
+  sunset: 'Sunset'
+}
+
 class SunEvent extends Component {
+
+  constructor(props) {
+    super(props)
+    this.event = evtDict[this.props.type]
+  }
 
   getActiveClass() {
     let classes = ['sunEvent', 'pure-g']
@@ -11,12 +23,16 @@ class SunEvent extends Component {
     return classes.join(' ')
   }
 
+  getEvent() {
+    return evtDict[this.props.type]
+  }
+  
   render() {
     return (
       <div className={this.getActiveClass()}>
         <div className="pure-u-1-2">
           <div className="eventType">
-            <p>{this.props.type}</p>
+            <p>{this.getEvent()}</p>
             <p className="eventDiff">{this.props.isToday ? this.props.diff.current : ''}</p>
           </div>
         </div>
